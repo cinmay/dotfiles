86c8d41887484bf31ddf5000a827acc2caadb22e
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2023-12-10 08:40:48 +0100

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -217,6 +217,78 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
+{
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = {
+    suggestion = { enabled = true },
+    panel = { enabled = true },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+  },
+},
+  {  
+  "stevearc/conform.nvim",
+  dependencies = { "mason.nvim" },
+  lazy = true,
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<leader>cF",
+      function()
+        require("conform").format({ formatters = { "injected" } })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
+  opts = function()
+  local plugin = require("lazy.core.config").plugins["conform.nvim"]
+  if plugin.config ~= M.setup then
+    Util.error({
+      "Don't set `plugin.config` for `conform.nvim`.\n",
+      "This will break **LazyVim** formatting.\n",
+      "Please refer to the docs at https://www.lazyvim.org/plugins/formatting",
+    }, { title = "LazyVim" })
+  end
+  ---@class ConformOpts
+  local opts = {
+    -- LazyVim will use these options when formatting with the conform.nvim formatter
+    format = {
+      timeout_ms = 3000,
+      async = false, -- not recommended to change
+      quiet = false, -- not recommended to change
+    },
+    ---@type table<string, conform.FormatterUnit[]>
+    formatters_by_ft = {
+      lua = { "stylua" },
+      fish = { "fish_indent" },
+      sh = { "shfmt" },
+    },
+    -- The options you set here will be merged with the builtin formatters.
+    -- You can also define any custom formatters here.
+    ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+      -- # Example of using dprint only when a dprint.json file is present
+      -- dprint = {
+      --   condition = function(ctx)
+      --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+      --   end,
+      -- },
+      --
+      -- # Example of using shfmt with extra args
+      -- shfmt = {
+      --   prepend_args = { "-i", "2", "-ci" },
+      -- },
+    },
+  }
+  return opts
+end
+}
+},
   --       Uncomment any of the lines below to enable them.
   -- require 'kickstart.plugins.autoformat',
   -- require 'kickstart.plugins.debug',
