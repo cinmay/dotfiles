609f9bfee3ce3099c19c46b73c9f8cd5873b2bc1
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2024-01-03 13:09:32 +0100

    added basic typescript sample project added font checking tools

---
+++ tools/typescriptTest/simple.js
@@ -0,0 +1,8 @@
+function sum(a, b) {
+    if (a === b) {
+        console.log("a is equal to b");
+        ;
+    }
+    return a + b;
+}
+module.exports = sum;
---
+++ tools/typescriptTest/simple.ts
@@ -0,0 +1,21 @@
+function sum(a, b) {
+
+  if (a === b) {
+    console.log("a is equal to b");
+    );
+    
+  }
+  else {
+    console.log("a is not equal to b");
+  }
+  if (a < b) {
+    cosole.log("" )
+    
+  }
+  // Print hellow world in the cosole.
+  console.log("  ");
+  
+  
+  return a + b;
+}
+module.exports = sum;
