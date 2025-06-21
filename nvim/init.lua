a0cefe46019363c6c09c5ca71d2ce409b5001b96
Gerhard Just-Olsen <cinmay05@gmail.com>
2025-06-21 16:26:22 +0200

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -719,7 +719,7 @@ require('mason-lspconfig').setup()
 --  define the property 'filetypes' to the map in question.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
   marksman = {},
