0f5b9bbf2594ae0949cc9693e4b6b7d9dde3b2ae
Gerhard Just-Olsen <cinmay05@gmail.com>
2026-05-13 21:47:00 +0200

    Fix: background transparency

--- omarchy/.config/nvim/init.lua
+++ omarchy/.config/nvim/init.lua
@@ -418,7 +418,10 @@ require("lazy").setup({
 			"MunifTanjim/nui.nvim",
 			{
 				"rcarriga/nvim-notify",
-				opts = { timeout = 3000 },
+				opts = {
+					timeout = 3000,
+					background_colour = "#000000",
+				},
 				config = function(_, opts)
 					require("notify").setup(opts)
 					vim.notify = require("notify")
